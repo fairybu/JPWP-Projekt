3fdc7d4e24dcc9e3ccd6e6b319bc7e6f43d0f5bd
fairy_bu <bulinska97@gmail.com>
2018-11-03 23:09:22 +0100

    Dodanie buttonów z otwierającymi sie oknami

--- app/src/main/java/com/example/firstapp/system_arabski_reg1.java
+++ app/src/main/java/com/example/firstapp/system_arabski_reg1.java
@@ -1,58 +1,28 @@
 package com.example.firstapp;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class system_arabski_reg1 extends AppCompatActivity {
 
-    public Button sys_arabski;
-    public Button sys_rzymski;
-    public Button sys_binarny;
+    public Button pow_ara_reg1;
 
-
-    public void sys_ara(){
-        sys_arabski= (Button)findViewById(R.id.sys_arabski);
-        sys_arabski.setOnClickListener(new View.OnClickListener() {
+    public void pow_ara(){
+        pow_ara_reg1=(Button)findViewById(R.id.pow_ara_reg1);
+        pow_ara_reg1.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent ot_ar = new Intent (MainActivity.this,system_arabski_reg1.class);
+                Intent pow_ara_reg=new Intent(system_arabski_reg1.this, MainActivity.class);
 
-                startActivity(ot_ar);
+                startActivity(pow_ara_reg);
             }
-        });
+        }));
     }
 
-    public void sys_rzy(){
-        sys_rzymski=(Button)findViewById(R.id.sys_rzymski);
-        sys_rzymski.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent ot_rzy= new Intent(MainActivity.this, system_rzymski_reg1.class);
-
-                startActivity(ot_rzy);
-            }
-        });
-
-    }
-
-    public void sys_bin(){
-        sys_binarny=(Button)findViewById(R.id.sys_binarny);
-        sys_binarny.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent ot_bin= new Intent(MainActivity.this,system_binarny_reg1.class);
-
-                startActivity(ot_bin);
-            }
-        });
-    }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -79,13 +49,10 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        sys_ara();
-        sys_rzy();
-        sys_bin();
+        setContentView(R.layout.activity_system_arabski_reg1);
+        pow_ara();
     }
 }
