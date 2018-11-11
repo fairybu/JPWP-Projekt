9248d5cf6b3027cafe6c36eed818cc3f3f0c7c61
fairy_bu <bulinska97@gmail.com>
2018-11-11 18:47:21 +0100

    próba zrobienia połączęń pomiędzy poszczególnymi oknami

---
+++ app/src/main/java/com/example/firstapp/run_2.java
@@ -0,0 +1,13 @@
+package com.example.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class run_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_2);
+    }
+}
